db72cfce15dfbfb5abc5daae3864a34dc4c677ca
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-05 21:08:58 +0300

    fix(Sidebar): Add click to show contact button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,22 @@
+import { GiClawSlashes } from 'react-icons/gi'
 import { josiah } from '../assets'
 const Sidebar = () => (
   <aside className='sidebar'>
     <div className='sidebar-info'>
+      {/* josiah */}
       <figure className='avatar-box'>
         <img src={josiah} alt='josiah' width={120} />
       </figure>
+      {/* name & title */}
       <div className='info-content'>
         <h1 className='name'>Josiah Nganga</h1>
         <p className='title'>Software Engineer</p>
       </div>
+      {/* click to show contact */}
+      <button className='info_more-btn'>
+        <span>Show Contacts</span>
+        <GiClawSlashes />
+      </button>
     </div>
   </aside>
 )
