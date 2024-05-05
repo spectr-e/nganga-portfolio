fbc52832e32af381f0359a4df6f340f0be00a711
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-05 10:51:27 +0300

    feat(About): Create about page starter file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
-const App = () => {
-  return <div>Hello bitches!</div>
-}
+const App = () => (
+  <div>
+    <p>Sidebar</p>
+    <div className='main-content'>
+      <nav>Navbar</nav>
+      <section
+        style={{
+          scrollBehavior: 'smooth',
+        }}
+      >
+        <Outlet />
+      </section>
+    </div>
+  </div>
+)
 
 export default App
