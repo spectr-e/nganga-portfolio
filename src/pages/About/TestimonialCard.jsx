b7f4b848dcdcfd5e6bca235adf7b0ca723ccb4ea
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-06 17:15:23 +0300

    fix(TestimonialCard): Add the avatars

--- src/pages/About/TestimonialCard.jsx
+++ src/pages/About/TestimonialCard.jsx
@@ -1,6 +1,14 @@
 const TestimonialCard = ({ data }) => {
   const { name, avatar, testimonial } = data
-  return <li className='testimonial-list'></li>
+  return (
+    <li className='testimonial-item'>
+      <div className='content-card'>
+        <figure className='testimonials-avatar-box'>
+          <img src={avatar} alt='avatar' width={60} />
+        </figure>
+      </div>
+    </li>
+  )
 }
 
 export default TestimonialCard
