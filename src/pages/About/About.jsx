4847421af174c66f6dd19eeadb16184b5c9bfcbf
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-06 16:34:08 +0300

    fix(About): Add an About Me section for the page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,7 +8,28 @@ const About = () => {
       .then((data) => setServices(data))
   }, [])
 
-  return <div className='about active'></div>
+  return (
+    <div className='about active'>
+      {/* about me */}
+      <header className=''>
+        <h2 className='h2 article-title'>About Me</h2>
+      </header>
+      <section className='about-text'>
+        <p className=''>
+          I am a Creative Full Stack Software Engineer from Nairobi, Kenya
+          working in web development and graphic design. I enjoy turning complex
+          problems into simple, beautiful and intuitive digital systems.
+        </p>
+        <p className=''>
+          My job is to build your website so that it is functional and
+          user-friendly but at the same time attractive. Moreover, I add a
+          personal touch to your product and make sure that it is eye-catching
+          and easy to use. My aim is to bring across your message and identity
+          in the most creative way
+        </p>
+      </section>
+    </div>
+  )
 }
 
 export default About
