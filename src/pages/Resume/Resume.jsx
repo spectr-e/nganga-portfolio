2dcdb9264bfad87e59b58e46ed722e517a0f8f89
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-06 18:08:26 +0300

    fix(Resume): Duplicate the section structure for experience

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,5 +1,6 @@
 import { FaBookReader } from 'react-icons/fa'
 import Timeline from './Timeline'
+import { BiSolidBookmark } from 'react-icons/bi'
 
 const Resume = () => {
   return (
@@ -7,18 +8,65 @@ const Resume = () => {
       <header className=''>
         <h2 className='h2 article-title'>My Resume</h2>
       </header>
+
+      {/* Education Section */}
       <div className='timeline'>
-        {/* title */}
         <div className='title-wrapper'>
-          {/* icon */}
           <div className='icon-box'>
             <FaBookReader />
           </div>
-          {/* text */}
           <h3 className='h3'>Education</h3>
         </div>
-
+        {/* details */}
         <ol className='timeline-list'>
+          <Timeline
+            title='University of Nairobi'
+            date={'2020 - 2024'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quasi esse, nulla mollitia quod expedita eveniet adipisci labore fugit? Excepturi atque quasi dicta. Sed fugit, saepe ipsa quo eligendi corporis?'
+            }
+          />
+          <Timeline
+            title='University of Nairobi'
+            date={'2020 - 2024'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quasi esse, nulla mollitia quod expedita eveniet adipisci labore fugit? Excepturi atque quasi dicta. Sed fugit, saepe ipsa quo eligendi corporis?'
+            }
+          />
+          <Timeline
+            title='University of Nairobi'
+            date={'2020 - 2024'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quasi esse, nulla mollitia quod expedita eveniet adipisci labore fugit? Excepturi atque quasi dicta. Sed fugit, saepe ipsa quo eligendi corporis?'
+            }
+          />
+        </ol>
+      </div>
+
+      {/* Experience Section */}
+      <div className='timeline'>
+        <div className='title-wrapper'>
+          <div className='icon-box'>
+            <BiSolidBookmark />
+          </div>
+          <h3 className='h3'>Experience</h3>
+        </div>
+        {/* details */}
+        <ol className='timeline-list'>
+          <Timeline
+            title='University of Nairobi'
+            date={'2020 - 2024'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quasi esse, nulla mollitia quod expedita eveniet adipisci labore fugit? Excepturi atque quasi dicta. Sed fugit, saepe ipsa quo eligendi corporis?'
+            }
+          />
+          <Timeline
+            title='University of Nairobi'
+            date={'2020 - 2024'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quasi esse, nulla mollitia quod expedita eveniet adipisci labore fugit? Excepturi atque quasi dicta. Sed fugit, saepe ipsa quo eligendi corporis?'
+            }
+          />
           <Timeline
             title='University of Nairobi'
             date={'2020 - 2024'}
