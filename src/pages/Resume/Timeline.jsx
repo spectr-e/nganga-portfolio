c32d633d9fbe4ba4ab997c6f25e367c1b5152bb2
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-06 18:03:33 +0300

    fix(Timeline): Create structure to arrange the props passed downl

--- src/pages/Resume/Timeline.jsx
+++ src/pages/Resume/Timeline.jsx
@@ -1,5 +1,9 @@
-const Timeline = () => {
-  return <div>Timeline</div>
-}
+const Timeline = ({ title, date, description }) => (
+  <li className='timeline-item'>
+    <h4 className='timeline-item-title h4'>{title}</h4>
+    <span>{date}</span>
+    <p className='timeline-text'>{description}</p>
+  </li>
+)
 
 export default Timeline
