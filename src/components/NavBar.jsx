61ec710ca56bc7d9d0542b9fdb51afc51ce265ae
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-06 16:17:27 +0300

    feat(NavBar): Add the navbar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,35 @@
-const NavBar = () => {
-  return <div>NavBar</div>
-}
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () => (
+  <nav className='navbar'>
+    <ul className='navbar-list'>
+      <li className='navbar-item'>
+        <NavLink to={'/'} className={'navbar-link'}>
+          About
+        </NavLink>
+      </li>
+      <li className='navbar-item'>
+        <NavLink to={'/resume'} className={'navbar-link'}>
+          Resume
+        </NavLink>
+      </li>
+      <li className='navbar-item'>
+        <NavLink to={'/projects'} className={'navbar-link'}>
+          Projects
+        </NavLink>
+      </li>
+      <li className='navbar-item'>
+        <NavLink to={'/blogs'} className={'navbar-link'}>
+          Blog
+        </NavLink>
+      </li>
+      <li className='navbar-item'>
+        <NavLink to={'/contacts'} className={'navbar-link'}>
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+)
 
 export default NavBar
