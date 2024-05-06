46bebb7b6df6b6222775726b8bb2bdf027d79a08
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-06 16:55:51 +0300

    fix(ServiceCard): Add icon, title, and description in a card design

--- src/pages/About/ServiceCard.jsx
+++ src/pages/About/ServiceCard.jsx
@@ -1,10 +1,14 @@
 const ServiceCard = ({ data }) => {
   const { title, icon, description } = data
-  console.log(icon)
   return (
-    <li className=''>
-      <div>
-        <img src={icon} alt='' />
+    <li className='service-item'>
+      <div className='service-icon-box'>
+        <img src={icon} alt='service icon' width={40} />
+      </div>
+
+      <div className='service-content-box'>
+        <h4 className='h4 service-item-title'>{title}</h4>
+        <p className='service-item-text'>{description}</p>
       </div>
     </li>
   )
