9dc2e2ded88566c9cc0f5888cf7a57973eba9d46
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-07 11:57:07 +0300

    fix(TestimonialCard): Edit class to add responsiveness to the card

--- src/pages/About/TestimonialCard.jsx
+++ src/pages/About/TestimonialCard.jsx
@@ -1,7 +1,7 @@
 const TestimonialCard = ({ data }) => {
   const { name, avatar, testimonial } = data
   return (
-    <li className='testimonial-item'>
+    <li className='testimonials-item'>
       <div className='content-card'>
         <figure className='testimonials-avatar-box'>
           <img src={avatar} alt='avatar' width={60} />
