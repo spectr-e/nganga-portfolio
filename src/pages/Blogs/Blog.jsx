33c497f8d63ce2d56a69f29f2e5ddb1da553a0b2
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-08 12:58:00 +0300

    fix(Blog): Add the blog meta detaills, category and date

--- src/pages/Blogs/Blog.jsx
+++ src/pages/Blogs/Blog.jsx
@@ -1,11 +1,21 @@
 const Blog = ({ post }) => {
   const { title, category, date, image, description } = post
   return (
-    <li className=''>
-      <a href='#' className=''>
-        <figure className=''>
+    <li className='blog-post-item'>
+      <a href='#'>
+        {/* blog image */}
+        <figure className='blog-banner-box'>
           <img src={image} alt='blog image' loading='lazy' />
         </figure>
+
+        {/* blog content */}
+        <div className='blog-content'>
+          <div className='blog-meta'>
+            <p className='blog-category'>{category}</p>
+            <span className='dot'></span>
+            <time datetime={date}>{date}</time>
+          </div>
+        </div>
       </a>
     </li>
   )
