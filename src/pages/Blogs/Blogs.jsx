9377dc0b676cd82c973e16c4c42aac9ab78d13fb
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-08 12:33:30 +0300

    fix(Blogs): Add page title

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,5 +1,11 @@
 const Blogs = () => {
-  return <div>Blogs</div>
+  return (
+    <section>
+      <header>
+        <h2 className='h2 article-title'>Projects</h2>
+      </header>
+    </section>
+  )
 }
 
 export default Blogs
