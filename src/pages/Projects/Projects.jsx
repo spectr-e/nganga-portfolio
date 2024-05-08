f757f49ec0274c123bbd0aafb987f6f885771692
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-05-08 11:51:44 +0300

    fix(Projects): Add the page title

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,5 +1,13 @@
 const Projects = () => {
-  return <div>Projects</div>
+  return (
+    <section className=''>
+      <header className=''>
+        <h2 className='h2 article-title'>Projects</h2>
+      </header>
+
+      {/* Filter Buttons */}
+    </section>
+  )
 }
 
 export default Projects
